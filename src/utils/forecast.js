e7019420cb61f4fee122221bf738674799ba0ee9
gabrielecloud <gabriel.olivari@ecloudsolutions.com>
2019-04-15 14:23:23 -0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = "https://api.darksky.net/forecast/d72fe5e79c23d4b600e3e9dc0c6244da/" + latitude + "," + longitude + "?units=si";
+
+  request({ url, json: true}, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to forecast services!", undefined);
+    } else if (body.error) {
+      callback("Coordinates error. Try another search.");
+    } else {
+      const temperature = body.currently.temperature;
+      const precipProbability = body.currently.precipProbability;
+      const todaySummary = body.daily.data[0].summary;
+      callback(undefined, `${todaySummary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
+    }
+  });
+};
+
+module.exports = forecast;
